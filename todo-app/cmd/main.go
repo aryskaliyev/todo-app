b02a005983d07a59409db63d44782d8846f1c419
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-10-07 16:54:23 +0600

    Working on todo-app

--- todo-app/cmd/main.go
+++ todo-app/cmd/main.go
@@ -9,16 +9,22 @@ import (
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatal("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
 
 func initConfig() error {
-	viper.AddConfigPath
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
