fdb5a98dd76969b1a4e63c7a1279c2bad4472a3b
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-10-07 13:17:27 +0600

    Working on todo-app

---
+++ todo-app/cmd/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"lincoln.boris/todo"
+	"lincoln.boris/todo/pkg/handler"
+	"log"
+)
+
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occurred while running http server: %s", err.Error())
+	}
+}
