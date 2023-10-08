85803f70eeb3042ed5ab0276883578e5667fe3dd
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-10-08 17:55:59 +0600

    Working on restapi. Video #10

--- todo-app/pkg/repository/todo_list_postgres.go
+++ todo-app/pkg/repository/todo_list_postgres.go
@@ -59,6 +59,16 @@ func (r *TodoListPostgres) GetById(userId, listId int) (todo.TodoList, error) {
 func (r *TodoListPostgres) Delete(userId, listId int) error {
 	query := fmt.Sprintf("DELETE FROM %s tl USING %s ul WHERE tl.id = ul.list_id AND ul.user_id=$1 AND ul.list_id=$2", todoListsTable, usersListsTable)
 	_, err := r.db.Exec(query, userId, listId)
-	
+
 	return err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		
+	}
 }
