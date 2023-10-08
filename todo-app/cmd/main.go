939783ee9ac9eb66d42f14b117cb7f71e67f5053
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-10-08 09:12:59 +0600

    Working on restapi

--- todo-app/cmd/main.go
+++ todo-app/cmd/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"github.com/sirupsen/logrus"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	"lincoln.boris/todo"
 	"lincoln.boris/todo/pkg/handler"
 	"lincoln.boris/todo/pkg/repository"
 	"lincoln.boris/todo/pkg/service"
-	"log"
+	"os"
 )
 
 func main() {
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+
 	if err := initConfig(); err != nil {
-		log.Fatalf("error initializing configs: %s", err.Error())
+		logrus.Fatalf("error initializing configs: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error loading env variables: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(repository.Config{
+		 Host: viper.GetString("db.host"),
+		 Port: viper.GetString("db.port"),
+		 Username: viper.GetString("db.username"),
+		 Password: os.Getenv("DB_PASSWORD"),
+		 DBName: viper.GetString("db.dbname"),
+		 SSLMode: viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		logrus.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
-		log.Fatalf("error occurred while running http server: %s", err.Error())
+		logrus.Fatalf("error occurred while running http server: %s", err.Error())
 	}
 }
 
--- todo-app/pkg/handler/auth.go
+++ todo-app/pkg/handler/auth.go
@@ -1,7 +1,18 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"lincoln.boris/todo"
+)
 
-func (h *Handler) signUp(c *gin.Context) {}
+func (h *Handler) signUp(c *gin.Context) {
+	var input todo.User
+
+	if err := c.BindJSON(&input); err != nil {
+		newErrorResponse(c, http.StatusBadRequest, err.Error())
+		return
+	}
+}
 
 func (h *Handler) signIn(c *gin.Context) {}
