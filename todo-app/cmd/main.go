e05ca4067eda263619d3e02a500d83d640464408
aryskaliyev <aryskaliyev@alumni.nu.edu.kz>
2023-10-09 13:58:17 +0600

    Working on restapi

--- todo-app/cmd/main.go
+++ todo-app/cmd/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/sirupsen/logrus"
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -10,6 +11,8 @@ import (
 	"lincoln.boris/todo/pkg/repository"
 	"lincoln.boris/todo/pkg/service"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -41,8 +44,26 @@ func main() {
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
-		logrus.Fatalf("error occurred while running http server: %s", err.Error())
+	go func () {
+		if err := srv.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			logrus.Fatalf("error occurred while running http server: %s", err.Error())
+		}
+	}()
+
+	logrus.Printf("TodoApp Started")
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
+	<-quit
+
+	logrus.Printf("TodoApp Shutting Down")
+
+	if err := srv.Shutdown(context.Background()); err != nil {
+		logrus.Errorf("error occurred on server shutting down: %s", err.Error())
+	}
+
+	if err := db.Close(); err != nil {
+		logrus.Errorf("error occurred on db connection close: %s", err.Error())
 	}
 }
 
